a64b5d48ed11f38ae49e8035e14c2ee0f8c65f4b
Ralph von der Heyden <ralph@rvdh.de>
2009-10-02 15:02:52 +0200

    added blank.html for disqus ajax support, adjusted path for deployment

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ruby _generate_tag_archive.rb
-rsync -rltPz --delete _site/ ralphvdh@blog.rvdh.de:domains/blog.rvdh.de/web/public
+rsync -rltPz --delete _site/ ralphvdh@rvdh.de:domains/rvdh.de/web/public
