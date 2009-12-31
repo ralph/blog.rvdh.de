eaae3e32b6fa3f657f23e3e6a890c6cc9e32a5c3
ralph <ralph@rvdh.de>
2009-12-31 13:42:58 +0100

    enable asset packaging again

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ruby generate_tag_archive.rb
-# rm _site/stylesheets/*.css
-# ./bin/jammit --config assets.yml --base-url 'http://rvdh.de' -o _site/stylesheets/
+rm _site/stylesheets/*.css
+./bin/jammit --config assets.yml --base-url 'http://rvdh.de' -o _site/stylesheets/
 rsync -rltPz --delete _site/ ralphvdh@rvdh.de:domains/rvdh.de/web/public
